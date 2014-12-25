7b3c064607e3e90a55377a09cbf1bd1a82183f15
baton2809 <butapro7@gmail.com>
2014-12-25 22:31:59 +0300

    Генерируем выборку из K - смеси Гауссиан

--- simselec.cpp
+++ simselec.cpp
@@ -1,69 +1,148 @@
 #include <iostream>
 #include <math.h>
+#include <random>
+#include <vector>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 const int N = (int)1e4;
-const int KConst = (int)1e2;
 
-double X[N][KConst];                // случайная величина
+double X[N];
 int mean, variance;                 // параметры: матожидание, дисперсия
-int nX;                             // размер выборки случайной величины
 int K;                              // кол-во смесей
+int T;                              // размер выборки случайной величины
+
+int gcd(int left, int right) {
+    return !right ? left : gcd(right, left % right);
+}
+
+int lcm(int left, int right) {
+    return left * right / gcd(left, right);
+}
+
+std::vector<int> getDenominators(std::vector<double> weights) {
+    std::vector<int> denomenators;
+    for (int i = 0; i < weights.size(); i++) {
+        denomenators.push_back(1 / weights[i]);
+    }
+    return denomenators;
+}
+
+// Find the LCM of the denominators of the probabilities
+int getL(std::vector<int> denominators) {
+    int right, left = 1;
+    while (!denominators.empty()) {
+        right = denominators.back();
+        denominators.pop_back();
+        left = lcm(left, right);
+    }
+    return left;
+}
+
+// Simulating a Loaded Die with a Fair Die
+int sample_categorical(std::vector<double> weights) {
+    std::vector<int> denoms = getDenominators(weights);
+    
+    // Initialization
+    // step 1
+    int L = getL(denoms);
+    
+    // step 2
+    std::vector<int> A(L);
+    
+    // step 3
+    int nextSideDie = 0;
+    for (int i = 0; i < weights.size(); i++) {
+        int nextSequence = L * weights[i];
+        for (int j = 0; j < nextSequence; j++) {
+            A[nextSideDie + j] = i;
+        }
+        nextSideDie += nextSequence;
+    }
+    
+    // Generation
+    // step 1
+    srand((unsigned int)time(0));
+    int S = rand() % L;
+    
+    // step 2
+    int k = A[S];
+    
+    return k;
+}
+
+double sample_gaussian(int mean, int variance) {
+    std::random_device randomDevice;
+    std::mt19937 generator(randomDevice());
+    std::normal_distribution<> distribution(mean, variance);
+    return distribution(generator);
+}
+
+std::vector<double> sample_mixture(std::vector<double> weights,    // \pi
+                                   std::vector<double> means,      // \mu
+                                   std::vector<double> variances,  // \Sigma
+                                   size_t T) {
+    std::vector<double> res(T);
+    for (size_t t = 0; t < T; ++t) {
+//        // кидаем "нечестный" кубик с указанными весами, можете сами подумать
+//        // как это сделать, а можете прочитать прекрасную статью по ссылке.
+        size_t k = sample_categorical(weights);
+//        // генерирует величину из нормального распределения с указанным средним
+//        // и дисперсией, рекомендую воспользоваться std::normal_distribution.
+        res[t] = sample_gaussian(means[k], variances[k]);
+    }
+
+    return res;
+}
+
+// Inverse transform sampling
+void genSeqOfNormDistr();
 
 int main(int argc, const char * argv[]) {
     srand((unsigned int)time(0));
     
-    // вспомогательные переменные
-    double R[N];                            // базовая случайная величина [0 1]
-    unsigned int a, m;                      // параметры для генерации базовой случ. вел.
-    double s = 0;                           // вспомогательная случайная величина
-    int nR;                                 // размер выборки базовой случайной величины
-
-    nR = 12;
-    nX = 10;
-    a = 9, m = 13;
-    K = 4;
+    T = 10;
+    K = 3;
     
-    // для каждой смеси
-    for (int k = 0; k < K; k++) {
-        
-        // инициализируем начальные значения
-        R[0] = (double)(rand())/RAND_MAX;
-        mean = rand() % nX + 1;
-        variance = rand() % nX + 1;
-        
-        // требуется генерация нескольких сл. базовых чисел R для получения одного нормального сл. значения
-        for (int j = 0; j < nX; j++) { // для каждого значения X[j][]
-            
-            for (int i = 0; i < nR; i++) {
-                // Lehmer multiplicative congruential generator (MCG). R - принадлежит отрезку [0 1]
-                R[i + 1] = fmod(fmod(a * R[i], m), 1);
-            }
-            
-            // Ниже - Моделирование нормальной случайной величины на основе центральной предельной теоремы
-            for (int i = 0; i < nR; i++) {
-                // случайная величина s ввиде суммы базовых чисел
-                s += R[i];
-            }
-            
-            // генерируем одно случайное число для каждой итерации
-            X[j][k] = mean + sqrt(variance) / (sqrt(nR / 12)) * (s - nR / 2);
-            
-            // в следующей итерации j: R[0] отлично от R[0] предыдущей итерации
-            R[0] = R[nR - 1];
-            
-            // обнуляем для корректной работы в след. итерации j
-            s = 0;
-        }
-    }
+    std::vector<double> res(T);
+    std::vector<double> weights(K);
+    std::vector<double> means(K);
+    std::vector<double> variances(K);
     
-    for (int i = 0; i < nX; i++) {
-        for (int k = 0; k < K; k++) {
-            cout << X[i][k] << " ";
-        }
-        cout << endl;
+    weights[0] = 0.2;
+    weights[1] = 0.3;
+    weights[2] = 0.5;
+    
+    means[0] = 1;
+    means[1] = 2;
+    means[2] = 5;
+    
+    variances[0] = 4;
+    variances[1] = 3;
+    variances[2] = 2;
+    
+    res = sample_mixture(weights, means, variances, T);
+    for (int i = 0; i < T; i++) {
+        cout << res[i] << "\n";
     }
     
     return 0;
+}
+
+void genSeqOfNormDistr() {
+    srand((unsigned int)time(0));
+    T = 10;
+    mean = rand() % T + 1;
+    variance = rand() % T + 1;
+    
+    /* Метод обратного преобразования */
+    double Y;
+    for (int j = 0; j < T; j++) {
+        Y = (double)(rand()) / RAND_MAX;
+        X[j] = mean + variance * sqrt(abs(-2 * logf(variance * Y * sqrt(2 * M_PI))));
+    }
+    for (int j = 0; j < T; j++) {
+        cout << X[j] << endl;
+    }cout << endl;
 }
