22909617dac84316e6329031732a3cce0845b4f9
baton2809 <butapro7@gmail.com>
2015-03-04 19:35:47 +0300

    Some changes

--- fwbam.py
+++ fwbam.py
@@ -3,88 +3,31 @@ __author__ = 'artiom'
 import time
 import pysam
 import math
+import numpy as np
 
 samfile = pysam.AlignmentFile("ex40.7.sorted.bam", "rb")
-
-lst = []
-for read in samfile.fetch('chr1'):
-    lst.append(read)
-reads_in_chr = len(lst)
-
 lengths_chr_list = samfile.lengths
 k = 100000
 
-bin = [0] * int(math.ceil(lengths_chr_list[0] / k) + 1)
-amount_of_bins = len(bin)
-
-def count_reads_naive_loop(N):
-    for i in range(N):
-        for read in samfile.fetch('chr1', i, i + 1):
-            bin[i / k] += 1
-
-    read_count = 0
-    for i in range(amount_of_bins):
-        read_count += bin[i]
-    print('amount of reads: {}'.format(read_count))
-
-def count_reads_quick_loop():
-    d = {}
-    for i in range(amount_of_bins):
-        d[i] = (i * k, (i + 1) * k - 1)  # look below in loop
-    #print(d)
-
-    am1, am2 = 0, 0
-    for i in range(amount_of_bins):  # 0,1,2...(n-1)
-        for read in samfile.fetch('chr1', i * k, (i + 1) * k - 1):  # amount of reads in each bin
-            am1 += 1
-
-    for read in samfile.fetch('chr1', 0, lengths_chr_list[0]):
-        am2 += 1
-
-    print('amount of reads: ({}, {})\n'.format(am1, am2))
-
-def explanation_test():
-    # Problem: Each read should star and end inside bin! if this condition doesn't perform the read will omit!
-
-    am1, am2, am = 0, 0, 0
-    for read in samfile.fetch('chr1', 10500000, 10600000):
-        am1 += 1
-
-    for read in samfile.fetch('chr1', 1060100, 10700000):
-        am2 += 1
-
-    for read in samfile.fetch('chr1', 10500000, 10699999):
-        am += 1
-
-    print('( {} , {} )\n'.format(am, am1+am2))
-
-    # my thoughts consist of additional counting of the reads that lie on the edge
-    # starting in the i-bin, and ending in (i+1)-bin
-    # but what length to take?
-
-def coverage_vector():
+def coverage_vector(name, order):
+    bin = np.zeros((int(math.ceil(lengths_chr_list[order] / k) + 1)), dtype=int)
     count = 0
-    for read in samfile.fetch('chr1', 0, lengths_chr_list[0]):
+    for read in samfile.fetch(name):
         bin[int(math.ceil(read.reference_start / k))] += 1
-
     for i in range(len(bin)):
         count += bin[i]
-    print('\n            reads: {}\nsum reads in bins: {}\n'.format(reads_in_chr, count))
+    print("reference name: %s\n" % name)
+    print('was reads: {}\ngot reads: {}\n'.format(samfile.count(samfile.references[order]), count))
     print("coverage vector:\n{}\n".format(bin))
 
-
 if __name__ == "__main__":
-
     start = time.time()
-
-    print('statistics:\n    length: {}\n     reads: {}\n      bins: {}\n'
-          .format(lengths_chr_list[0], reads_in_chr, amount_of_bins))
-    # naive_loop(100000)
-    #count_reads_quick_loop()
-    #explanation_test()
-    coverage_vector()
-
-
+    # print('statistics:\n    length: {}\n     reads: {}\n      bins: {}\n'
+    #       .format(lengths_chr_list[0], reads_in_chr, amount_of_bins))
+    order = 0
+    for name_reference in samfile.references:
+        coverage_vector(name_reference, order)
+        order += 1
     end = time.time()
     print('time: {} sec'.format(round((end - start), 2)))
 
