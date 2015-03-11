e25cc595e74ab902d577819feb139d94070133f1
baton2809 <butapro7@gmail.com>
2015-03-11 18:04:29 +0300

    e-step, m-step: lambda

--- em_poisson_mix.py
+++ em_poisson_mix.py
@@ -19,13 +19,28 @@ def loglikelihood():
     return np.sum(np.log(poisson_mixture_model))
 
 def e_step():
-    pass
+    for n in range(N):
+        for k in range(K):
+            gamma[n][k] = pi[k] * poisson.pmf(x[n], lambda_parameter[k]) / \
+                np.sum(pi[j] * poisson.pmf(x[n], lambda_parameter[j]) for j in range(K))
+    # I don't satisfy with almost the same posterior probabilities on each [n] step.
+    # Does the algorithm work correctly?
+    return gamma
 
 def m_step():
-    pass
+    Nk = gamma.sum(axis=0)
+    for k in range(K):
+        lambda_parameter[k] = np.dot(gamma.T[k], x) / Nk[k]
+
+    # \hat \pi to be continued...
 
 if __name__ == "__main__":
     lambda_parameter = np.random.uniform(0.0, 5.0, K)
     pi = np.random.dirichlet(np.ones(K), size=None)
     x = data_generator()
-    print("The Log Likelihood : ", loglikelihood())
+    # print("The Log Likelihood : ", loglikelihood())
+    gamma = np.zeros((N, K))
+    gamma = e_step()
+    print('old lambda: %a' % lambda_parameter)
+    m_step()
+    print('new lambda: %a' % lambda_parameter)
