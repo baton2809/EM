f8e3eafc013b3de210f4b3269d40a27d90ae9cbb
baton2809 <butapro7@gmail.com>
2015-04-30 18:01:29 +0300

    trouble with likelihood counting
    the log likelihood not increases

--- fdr.py
+++ fdr.py
@@ -26,16 +26,52 @@ if __name__ == "__main__":
         N = len(gamma)
 
         p0 = gamma[:, 0]
-        p1 = gamma[:, 1]
+        log_p0 = np.log(gamma[:, 0])
 
-        E_FP, E_TP = 0, 0
-        for i in range(N):
-            if (p1[i] > 0.5):
-                E_FP += p1[i]*p0[i]
-                E_TP += p1[i]*p1[i]
+        acc, count = 0.0, 0
+        mask = log_p0 <= np.log(.5)
+        res = log_p0 * mask
+        res[res == 0] = -np.inf
+        acc = logsumexp(res)
+        count = mask.sum()
+        mFDR = np.exp(acc - np.log(count))
 
-        mFDR = E_TP / (E_TP + E_FP)
+        # acc = logsumexp()
+        # count = mask.sum()
+        # for i in range(N):
+        #     if p0[i] <= 0.5:
+        #         acc = np.logaddexp(acc, log_p0[i])
+        #         # acc += p0[i]
+        #         count += 1
+        # acc = np.exp(acc)
+        # mFDR = acc / count
+
+
         nums = l[-2:]
         if 'a' in nums:
             nums = nums.replace('a', '')
         print('chr{} - {}'.format(nums, mFDR))
+
+# chr0 - 1.1995865205217623e-42
+# chr1 - 0.13558515922491945
+# chr10 - 1.2940796768728552e-05
+# chr11 - 0.17127440111106051
+# chr13 - 0.08388966940863529
+# chr14 - 0.05473103217927941
+# chr15 - 0.000200033381191717
+# chr16 - 0.0
+# chr17 - 2.298567884015194e-16
+# chr18 - 4.349472225605853e-59
+# chr19 - 0.14482518546808315
+# chr2 - 0.1610207087122315
+# chr20 - 0.21212532115651572
+# chr21 - 0.11659640739539245
+# chr22 - 7.792327121953088e-05
+# chr23 - 1.5684180698918506e-31
+# chr3 - 0.11478039970067258
+# chr4 - 4.210468489140651e-10
+# chr5 - 3.5086242009348787e-175
+# chr6 - 0.007639271299803457
+# chr7 - 0.10104421208138556
+# chr8 - 0.13787491163044366
+# chr9 - 2.2472573134346117e-07
--- hmm.py
+++ hmm.py
@@ -5,70 +5,56 @@ import bisect
 from scipy.stats import poisson
 from scipy.misc import logsumexp
 import glob
+import time
+import warnings
+warnings.filterwarnings("error")
 
 GAMMA_FILE = './data/gamma/gamma20'
 LAMBDA_FILE = './data/lambda/lambda20'
 PI_FILE = './data/pi/pi20'
 
-# N = 300
 K = 2
 
-def forward_backward():
-    # alpha[0, :] = pi * poisson.pmf(x[0], lambda_parameter)  # basis
+def forward_backward(pois):
+    # alpha[0, :] = np.log(pi) + poisson.logpmf(x[0], lambda_parameter)  # basis
     # for i in range(1, N):   # recursion
-    #     alpha[i, :] = poisson.pmf(x[i], lambda_parameter) * (alpha[i-1, 0] * A[0, ] + alpha[i-1, 1] * A[1, ])
-    #
-    # beta[N-1] = np.ones(K)
-    # for i in range(N-2, 0, -1):     # recursion
-    #     beta[i, ] = np.sum(beta[i+1, ] * poisson.pmf(x[i+1], lambda_parameter) * A, axis=1)
-
-    alpha[0, :] = np.log(pi) + poisson.logpmf(x[0], lambda_parameter)  # basis
+    #     alpha[i, ] = poisson.logpmf(x[i], lambda_parameter) + logsumexp(alpha[i-1, ] + np.log(A), axis=1)
+    alpha[0, :] = np.log(pi) + pois[0, ]  # basis
     for i in range(1, N):   # recursion
-        alpha[i, ] = poisson.logpmf(x[i], lambda_parameter) + logsumexp(alpha[i-1, ] + np.log(A), axis=1)
+        alpha[i, ] = pois[i, ] + logsumexp(alpha[i-1, ] + np.log(A), axis=1)
 
     beta[N-1] = np.log(np.ones(K))  # basis
-    for i in range(N-2, 0, -1):     # recursion
-        beta[i, ] = logsumexp(beta[i+1, ] + np.log(A) + poisson.logpmf(x[i+1], lambda_parameter), axis=1)
+    # for i in range(N-2, -1, -1):     # recursion
+    #     beta[i, ] = logsumexp(beta[i+1, ] + np.log(A.T) + poisson.logpmf(x[i+1], lambda_parameter), axis=1)
+    for i in range(N-2, -1, -1):     # recursion
+        beta[i, ] = logsumexp(beta[i+1, ] + np.log(A.T) + pois[i+1, ], axis=1)
 
     return alpha, beta  # actually it is log alpha and log beta
 
-def e_step(log_alpha, log_beta):
-    # gamma = alpha * beta / np.array(alpha * beta).sum(axis=1)[:, np.newaxis]
-
-    gamma = log_alpha + log_beta - logsumexp(log_alpha + log_beta, axis=1)[:, np.newaxis]
+def e_step(log_alpha, log_beta, pois):
+    gamma = log_alpha + log_beta
+    gamma -= logsumexp(gamma, axis=1)[:, np.newaxis]
     np.exp(gamma, gamma)
 
-    denom = np.zeros(N)
-
-    # log_A = np.log(A)
-    # log_pois = np.array([poisson.logpmf(x, lambda_parameter[t]) for t in range(K)]).T
-    # for t in range(N):
-    #     for i in range(K):
-    #         for j in range(K):
-    #             denom[t] += log_alpha[t-1, i] + log_A[i, j] + log_pois[t, j] + log_beta[t, j]
-    #
-    #     for i in range(K):
-    #         for j in range(K):
-    #             xi[t, i, j] = log_alpha[t-1, i] + log_A[i, j] + log_pois[t, j] + log_beta[t, j] - logsumexp(denom[t])
-    # np.exp(xi, xi)
-
-    alpha = np.exp(log_alpha)
-    beta = np.exp(log_beta)
-    for t in range(N):
+    for t in range(1, N):
         for i in range(K):
             for j in range(K):
-                denom[t] += alpha[t-1, i] * A[i, j] * poisson.pmf(x[t], lambda_parameter[j]) * beta[t, j]
-                # TROUBLE: denom = [0. .. 0.] <= вырожденные alpha, beta
-        for i in range(K):
-            for j in range(K):
-                xi[t, i, j] = alpha[t-1, i] * A[i, j] * poisson.pmf(x[t], lambda_parameter[j]) * beta[t, j] / denom[t]
+                # xi[t, i, j] = (log_alpha[t-1, i] + np.log(A[i, j]) +
+                #                poisson.logpmf(x[t], lambda_parameter[j]) +
+                #                log_beta[t, j])
+                xi[t, i, j] = (log_alpha[t-1, i] + np.log(A[i, j]) +
+                               pois[t, j] +
+                               log_beta[t, j])
 
+        xi[t] -= logsumexp(xi[t])
+    np.exp(xi[1:], xi[1:])
     return gamma, xi
 
 def baum_welch(gamma, xi):
-
     pi = gamma[0, ]
-    A = np.sum(xi[1:, ], axis=0) / np.sum(gamma[1:, ], axis=0)
+    print(pi.sum())
+    A = np.sum(xi[1:, ], axis=0)
+    A /= A.sum(axis=1)[np.newaxis].T
     Nk = np.sum(gamma, axis=0)
     for k in range(K):
         lambda_parameter[k] = np.dot(gamma.T[k], x) / Nk[k]
@@ -76,23 +62,79 @@ def baum_welch(gamma, xi):
     return pi, A, lambda_parameter
 
 def ll():
-    pass
+    return logsumexp(alpha[-1])
+
+def poissons():
+    """
+    To ease calculation and avoid repeated actions evaluating the Poisson's function in some methods
+    :return: avector of poisson's probabilities
+    """
+    pois = np.zeros((N, K))
+    for k in range(K):
+        pois[:, k] = poisson.logpmf(x[:], lambda_parameter[k])
+    return pois
+
+def check_gamma():
+    """
+    For debugging
+    :return: True if each summation over states for gamma get 1.
+    """
+    t = gamma.sum(axis=1)
+    print(np.size(t[t/1 - 1 > 10**-5]) == 0)
 
 if __name__ == "__main__":
+    start = time.time()
     x = np.loadtxt("covvec", dtype=int)     # 20 bin
-
     N = len(x)
     alpha = np.zeros((N, K))
     beta = np.zeros((N, K))
     gamma = np.zeros((N, K))
     xi = np.zeros((N, K, K))
 
-    lambda_parameter = [267.69811321, 1192.93333333]
-    pi = [0.18327561, 0.81672439]
+    lambda_parameter = [4.45536015, 0.54416903]
 
-    A = np.array([[0.3, 0.7], [0.9, 0.1]])  # how to init correctly transition probability?
+    pi = [0.89115594, 0.10884406]
+    A = np.full((K, K), 1/K)
+    pois = poissons()
 
-    alpha, beta = forward_backward()
-    gamma, xi = e_step(alpha, beta)
-    pi, A, lambda_parameter = baum_welch(gamma, xi)
-    print(A)
+    ll_new = ll()
+    tolerance = 10**-3
+    iter = 0
+    while True:
+        print("step: {}, likelihood = {}\n".format(iter, ll_new))
+        # print("pi = {}\n A = {}\nlambda = {}".format(pi, A, lambda_parameter))
+        iter += 1
+        alpha, beta = forward_backward(pois)
+        gamma, xi = e_step(alpha, beta, pois)
+        check_gamma()
+        pi, A, lambda_parameter = baum_welch(gamma, xi)
+
+        ll_old = ll()
+        if np.absolute(ll_new / ll_old - 1) < tolerance:
+            break
+        else:
+            ll_new = ll_old
+    end = time.time()
+    print('Iteration was made before convergence: {}\ntime : {} sec'.format(iter, round(end-start, 2)))
+
+# TROUBLE in loglikelihood
+
+# True                                          gamma.sum(axis=1) = 1.
+# 1.00000000001                                 pi.sum()
+# step: 1, likelihood = -414746.69618496817
+#
+# True                                          gamma.sum(axis=1) = 1.
+# 1.0                                           pi.sum()
+# step: 2, likelihood = -365862.073677846
+#
+# True                                          gamma.sum(axis=1) = 1.
+# 1.00000000001                                 pi.sum()
+# step: 3, likelihood = -360476.2153981096
+#
+# True                                          gamma.sum(axis=1) = 1.
+# 0.999999999974                                pi.sum()
+# step: 4, likelihood = -367195.59363218537
+#
+# True                                          gamma.sum(axis=1) = 1.
+# 1.0                                           pi.sum()
+# step: 5, likelihood = -377342.47137207724
