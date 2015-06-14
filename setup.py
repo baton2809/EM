f13a6d74913229539a4828c0d3c60c7de158c134
Sergei Lebedev <superbobry@gmail.com>
2015-06-14 14:23:45 +0300

    MAINT cleaned up ``setup.py``

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2015 Artiom Butomov <butapro7@gmail.com>
 
-"""A tool for comparing of biological data in Python with HMM."""
+"""A tool of comparing and analyzing FAIRE-seq data."""
 
 import sys
 
@@ -10,48 +10,45 @@ from setuptools import setup, Extension
 
 
 DISTNAME = "fairy"
-DESCRIPTION = "A tool of comparing and analyzing"
+DESCRIPTION = __doc__
 LONG_DESCRIPTION = open("README.rst").read()
-MAINTAINER = "Artiom Butomov"
-MAINTAINER_EMAIL = "butapro7@gmail.com"
+AUTHOR = "Artiom Butomov"
+AUTHOR_EMAIL = "butapro7@gmail.com"
 LICENSE = "new BSD"
 
 CLASSIFIERS = [
-               "Development Status :: 3 - Alpha",
-               "License :: OSI Approved",
-               "Intended Audience :: Developers",
-               "Intended Audience :: Science/Research",
-               "Topic :: Software Development",
-               "Topic :: Scientific/Engineering",
-               "Programming Language :: Cython",
-               "Programming Language :: Python",
-               "Programming Language :: Python :: 2",
-               "Programming Language :: Python :: 2.6",
-               "Programming Language :: Python :: 2.7",
-               "Programming Language :: Python :: 3",
-               "Programming Language :: Python :: 3.3",
-               "Programming Language :: Python :: 3.4",
-               ]
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "Topic :: Scientific/Engineering",
+    "Programming Language :: Cython",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+]
 
 import fairy
 
 VERSION = fairy.__version__
 
 setup_options = dict(
-                     name="fairy",
-                     version=VERSION,
-                     description=DESCRIPTION,
-                     long_description=LONG_DESCRIPTION,
-                     maintainer=MAINTAINER,
-                     maintainer_email=MAINTAINER_EMAIL,
-                     license=LICENSE,
-                     url="https://github.com/baton2809/thesis",
-                     packages=["fairy"],
-                     classifiers=CLASSIFIERS,
-                     ext_modules=[
-                                  Extension("fairy._speedups", ["fairy/_speedups.c"])
-                                  ]
-                     )
+    name="fairy",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    url="https://github.com/baton2809/thesis",
+    packages=["fairy"],
+    classifiers=CLASSIFIERS,
+    ext_modules=[
+        Extension("fairy._speedups", ["fairy/_speedups.c"])
+    ]
+)
 
 
 # For these actions, NumPy is not required. We want them to succeed without,
